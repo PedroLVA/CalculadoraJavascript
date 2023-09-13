f12bab4a5216a8dc08688975afbc7bf2f2ab0b46
Pedro Athayde <pedro_athayde@outlook.com>
2023-09-13 13:28:21 -0300

    feat: added the logic and error correction

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 
+let visor = document.getElementById('visor');
+let resposta = false;
+
 //Funções
-function insert(numero){
-    let textVisor = document.getElementById('visor').innerHTML;
-    if(textVisor === "0"){
-        document.getElementById('visor').innerHTML = "";
-        document.getElementById('visor').innerHTML += numero;
+function insert(numero) {
+
+    let textVisor = visor.innerHTML;
+    if (textVisor === "0" || resposta == true) {
+        visor.innerHTML = "";
+        visor.innerHTML += numero.toLocaleString('en-US');
+        resposta = false;
     }
-    else{
-        document.getElementById('visor').innerHTML += numero;
+    else {
+        visor.innerHTML += numero.toLocaleString('en-US');
+        resposta = false;
     }
+}
+
+function apagaVisor() {
+    resposta = false;
+    visor.innerHTML = "0";
+}
+
+function calcular() {
+    if (visor) {
+        let resultado = eval(visor.innerHTML);
+        resultado = parseFloat(resultado.toFixed(10));
+        resposta = true;
+        document.getElementById('visor').innerHTML = resultado.toLocaleString('en-US');
     }
-    
-function apagaVisor(){
-    document.getElementById('visor').innerHTML = "0";
 }
