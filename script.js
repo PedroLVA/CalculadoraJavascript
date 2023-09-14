9e908407d98cefb331414dba4bd59da42e7810cd
Pedro Athayde <pedro_athayde@outlook.com>
2023-09-13 23:25:54 -0300

    feat: added light and dark theme

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 
 let visor = document.getElementById('visor');
 let resposta = false;
+var icon = document.getElementById('icon');
+
+
 
 //Funções
+
+
+
+//light and darkmode
+
+icon.onclick = function(){
+    document.body.classList.toggle("light-theme");
+    if (document.body.classList.contains("light-theme")){
+        icon.src ="SunIcon.png"
+    }
+    else{
+        icon.src ="MoonIcon.png"
+    }
+}
+
+//calculadora
 function insert(numero) {
 
     let textVisor = visor.innerHTML;
-    if (textVisor === "0" || resposta == true) {
+    if (textVisor === "0" || resposta) {
         visor.innerHTML = "";
-        visor.innerHTML += numero.toLocaleString('en-US');
+        visor.innerHTML += numero;
         resposta = false;
     }
     else {
-        visor.innerHTML += numero.toLocaleString('en-US');
+        visor.innerHTML += numero;
         resposta = false;
     }
 }
@@ -27,6 +46,6 @@ function calcular() {
         let resultado = eval(visor.innerHTML);
         resultado = parseFloat(resultado.toFixed(10));
         resposta = true;
-        document.getElementById('visor').innerHTML = resultado.toLocaleString('en-US');
+        document.getElementById('visor').innerHTML = resultado;
     }
 }
